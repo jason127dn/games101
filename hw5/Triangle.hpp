3c04e63a323cf631f218101fe5bd9c817c1bb6c1
jason127dn <jason127dn@gmail.com>
2022-02-04 15:33:28 +0800

    [hw5] Moller-Trumbore

--- hw5/Triangle.hpp
+++ hw5/Triangle.hpp
@@ -11,7 +11,21 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
-    return false;
+    Vector3f e1 = v1-v0;
+    Vector3f e2 = v2-v0;
+    Vector3f s  = orig-v0;
+    Vector3f s1 = crossProduct(dir,e2);
+    Vector3f s2 = crossProduct(s,e1);
+    float dev = 1./ dotProduct(s1,e1);
+    float t = dotProduct(s2,e2)*dev;
+    float b1 = dotProduct(s1,s)*dev;
+    float b2 = dotProduct(s2,dir)*dev;
+    tnear = t;
+    u = b1;
+    v = b2;
+    if(t>0 && 0<b1 && b1<1 && 0<b2 && b2<1 && 0<1-b1-b2 && 1-b1-b2<1)
+        std::cout << orig << "orig\n" << dir << "dir\n" << tnear << "tnear\n" << b1 << "b1\n" << b2 <<"b2\n";  
+    return t>0 && 0<b1 && b1<1 && 0<b2 && b2<1 && 0<1-b1-b2 && 1-b1-b2<1;
 }
 
 class MeshTriangle : public Object
